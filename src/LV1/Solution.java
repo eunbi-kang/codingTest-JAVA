add4b206316767849c2b6602fd8a5927709a9f17
eunbi-kang <ebzzang1905@naver.com>
2024-06-21 16:54:04 +0900

    LV1-코딩테스트 연습>스택/큐>같은 숫자는 싫어

--- src/LV1/Solution.java
+++ src/LV1/Solution.java
@@ -4,22 +4,38 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         int[] answer = {};
-        arr = Arrays.stream(arr).distinct().toArray();
+        // arr = Arrays.stream(arr).distinct().toArray();
         List<Integer> newArr = new ArrayList<>();
-
-        for(int i=0; i<arr.length;i++) {
-            if(arr[i] == arr[i+1]){
-                arr[i+1] = arr[i];
-                newArr.add(arr[i+1]);
-                // arr = Arrays.copyOfRange(arr, i+1, arr.length);
-                
-            } else { 
+        
+        // for(int i=0; i<=arr.length;i++) {
+        //     if(arr[i] == arr[i+1]){
+        //         continue;
+        //         // arr = Arrays.copyOfRange(arr, i+1, arr.length);
+        //     } else { 
+        //         newArr.add(arr[i]);
+        //         // arr = Arrays.copyOfRange(arr, i, arr.length);
+        //     }
+            
+        // }
+        int i=0;
+        while(true){
+            if(i ==arr.length-1){
                 newArr.add(arr[i]);
-                // arr = Arrays.copyOfRange(arr, i, arr.length);
+                break;
             }
+            if(arr[i]== arr[i+1]){
+                i++;
+            } else {
+                newArr.add(arr[i]);
+                i++;
+            }
+            // System.out.println(newArr);
+        }
+        
+        for(int j=0; j<newArr.size();j++){
             System.out.println(newArr);
         }
-       
+
 
         return answer;
     }
